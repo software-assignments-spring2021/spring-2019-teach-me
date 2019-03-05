f8838db894609a3c4380bb5c00262dc64543294a
Haomiao Han <hh1519@nyu.edu>
2019-03-04 21:30:56 -0500

    adding new unit test & enabling 'npm test'

--- test/test.js
+++ test/test.js
@@ -2,11 +2,19 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('the server', function() {
-   it('should respond to a request for / with a html document', function (done) {
-       request(app)
-           .get('/')
-           .set('Accept', 'text/html')
-           .expect('Content-Type', /html/)
-           .expect(200, done);
-   });
+	it('should respond to a request for / with a html document', function (done) {
+		request(app)
+			.get('/')
+			.set('Accept', 'text/html')
+			.expect('Content-Type', /html/)
+			.expect(200, done);
+	});
+
+	it('should repsond to /api/classes with response type json', function (done) {
+		request(app)
+			.get('/api/classes')
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200, done);
+	});
 });
