438ed152fa9c4d8503508793f85998c387a33b2a
Junyang Ma <ma.junyang328@gmail.com>
2019-03-08 20:54:42 -0500

    Creating /create-class page

---
+++ src/views/CreateClass/CreateClass.js
@@ -0,0 +1,24 @@
+//https://www.robinwieruch.de/react-fetching-data/
+import React, { Component } from 'react';
+
+import './CreateClass.css'
+
+// class CreateClass extends Component {
+// 	constructor(props) {
+// 		super(props);
+// 	}
+
+// 	render() {
+// 		return (
+// 			<h1>Hello World</h1>
+// 		);
+// 	}
+// }
+
+const CreateClass = props => {
+	return (
+		<h1>Hello</h1>
+	);
+};
+
+export default CreateClass;
