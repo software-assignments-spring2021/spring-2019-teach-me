2f62f2da2f825d37ac1559cac0b1b791eaa1fa87
Junyang Ma <ma.junyang328@gmail.com>
2019-03-30 21:20:10 -0400

    Adding handleCancel function and modifying the css file for create-class page

--- src/views/CreateClass/CreateClass.js
+++ src/views/CreateClass/CreateClass.js
@@ -5,11 +5,14 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 
 import './CreateClass.css'
 
-
 class CreateClass extends Component {
+
 	constructor(props) {
 		super(props);
+		
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleCancel = this.handleCancel.bind(this);
+
 		this.state = {
 			successRedirect: undefined
 		};
@@ -37,11 +40,14 @@ class CreateClass extends Component {
 		}).then(response => response.json())
 			.then(data => this.setState({successRedirect: data}));
 			
+	}
 
+	handleCancel(e) {
+		this.setState({successRedirect: {"result": "cancelled"}});
 	}
 
 	render() {
-		if (this.state.successRedirect && this.state.successRedirect.result === 'success') {
+		if (this.state.successRedirect && (this.state.successRedirect.result === 'success' || this.state.successRedirect.result === 'cancelled')) {
 			return (
 				<Redirect to='/home' />
 			)
@@ -50,19 +56,19 @@ class CreateClass extends Component {
 			return (
 				<div id='submit-form'>
 					<h3>Create a Class</h3>
-						<form onSubmit={this.handleSubmit}>
-							<label>Name</label><br />
-							<input type="text" name="name" required /><br />
-							<label>Description</label><br />
-							<input type="text" name="description" required /><br />
-							<label>Price</label><br />
-							<input type="number" name="price" required /><br />
-							<label>Proposed Schedule</label><br />
-							<input type="text" name="proposedSchedule" required /><br />
-							<label>Category</label><br />
-							<input type="text" name="category" required /><br />
-							<input type="submit" value="Publish" />
-							<input type="button" value="Cancel" />
+					<form onSubmit={this.handleSubmit}>
+						<label>Name</label><br />
+						<input type="text" name="name" required /><br />
+						<label>Description</label><br />
+						<input type="text" name="description" required /><br />
+						<label>Price</label><br />
+						<input type="number" name="price" required /><br />
+						<label>Proposed Schedule</label><br />
+						<input type="text" name="proposedSchedule" required /><br />
+						<label>Category</label><br />
+						<input type="text" name="category" required /><br />
+						<input type="submit" value="Publish" />
+						<input type="button" value="Cancel" onClick={this.handleCancel} />
 					</form>
 				</div>
 			);
