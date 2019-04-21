326fd6c3a9d37099ca710bbb2e83ab23edf92930
Sameer Tulshyan <sameer.tulshyan@gmail.com>
2019-04-20 23:20:34 -0400

    Adding validator for url

--- src/views/CreateClass/CreateClass.js
+++ src/views/CreateClass/CreateClass.js
@@ -1,20 +1,21 @@
 //https://www.robinwieruch.de/react-fetching-data/
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 //import { registerUser } from "../../actions/authActions";
 import classnames from "classnames";
+import validator from 'validator';
 
-import './CreateClass.css'
+
+import "./CreateClass.css";
 
 class CreateClass extends Component {
-
 	constructor(props) {
 		super(props);
-		
+
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleCancel = this.handleCancel.bind(this);
 		this.handleUrl = this.handleUrl.bind(this);
@@ -25,18 +26,17 @@ class CreateClass extends Component {
 	}
 
 	componentDidMount() {
-    // If not logged in and user navigates to Create Class page, should redirect them to login
-	    if (this.props.auth.isAuthenticated) {
+		// If not logged in and user navigates to Create Class page, should redirect them to login
+		if (this.props.auth.isAuthenticated) {
 			//console.log(this.props.auth);
-		}
-		else {
+		} else {
 			this.props.history.push("/login");
 		}
-  	}
+	}
 
-  	handleUrl(e) {
-  		
-  	}
+	handleUrl(e) {
+
+	}
 
 	handleSubmit(e) {
 		e.preventDefault();
@@ -46,51 +46,68 @@ class CreateClass extends Component {
 		for (let userInput of newClassData.entries()) {
 			newClassObj[userInput[0]] = userInput[1];
 		}
-		
+
 		const instructorId = this.props.auth.user.id;
 		newClassObj.instructorId = instructorId;
 		console.log(newClassObj);
-		
-		fetch('/api/create-class/', {
-			method: 'POST',
+
+		fetch("/api/create-class/", {
+			method: "POST",
 			body: JSON.stringify(newClassObj),
-            headers: {
-            	'Content-Type': 'application/json'
-            }
-		}).then(response => response.json())
-			.then(data => this.setState({successRedirect: data}));
-			
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(response => response.json())
+			.then(data => this.setState({ successRedirect: data }));
 	}
 
 	handleCancel(e) {
-		this.setState({successRedirect: {"result": "cancelled"}});
+		this.setState({ successRedirect: { result: "cancelled" } });
 	}
 
 	render() {
-		if (this.state.successRedirect && (this.state.successRedirect.result === 'success' || this.state.successRedirect.result === 'cancelled')) {
+		if (
+			this.state.successRedirect &&
+			(this.state.successRedirect.result === "success" ||
+				this.state.successRedirect.result === "cancelled")
+		) {
+			return <Redirect to="/class-history" />;
+		} else {
 			return (
-				<Redirect to='/class-history' />
-			)
-		}
-		else {
-			return (
-				<div id='submit-form'>
+				<div id="submit-form">
 					<h3>Create a Class</h3>
 					<form onSubmit={this.handleSubmit}>
-						<label>Name</label><br />
-						<input type="text" name="name" required /><br />
-						<label>Description</label><br />
-						<input type="text" name="description" required /><br />
-						<label>Price</label><br />
-						<input type="number" name="price" required /><br />
-						<label>Proposed Schedule</label><br />
-						<input type="text" name="proposedSchedule" required /><br />
-						<label>Category</label><br />
-						<input type="text" name="category" required /><br />
-						<label>Payment Link</label><br />
-						<input type="text" name="paymentLink" /><br />
+						<label>Name</label>
+						<br />
+						<input type="text" name="name" required />
+						<br />
+						<label>Description</label>
+						<br />
+						<input type="text" name="description" required />
+						<br />
+						<label>Price</label>
+						<br />
+						<input type="number" name="price" required />
+						<br />
+						<label>Proposed Schedule</label>
+						<br />
+						<input type="text" name="proposedSchedule" required />
+						<br />
+						<label>Category</label>
+						<br />
+						<input type="text" name="category" required />
+						<br />
+						<label>Payment Link</label>
+						<br />
+						<input type="text" onBlur={this.handleUrl} name="paymentLink" />
+						<br />
 						<input type="submit" value="Publish" />
-						<input type="button" value="Cancel" onClick={this.handleCancel} />
+						<input
+							type="button"
+							value="Cancel"
+							onClick={this.handleCancel}
+						/>
 					</form>
 				</div>
 			);
