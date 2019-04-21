4563750e3e7f7ff64182c6925fd8eee4ead74659
Sameer Tulshyan <sameer.tulshyan@gmail.com>
2019-04-21 00:03:42 -0400

    Adding payment url to class details page

--- src/views/ClassDetail/ClassDetail.js
+++ src/views/ClassDetail/ClassDetail.js
@@ -315,6 +315,8 @@ class ClassDetail extends Component {
                                 <p>{classData.proposedSchedule}</p>
                                 <h3>Class Category</h3>
                                 <p>{classData.category}</p>
+                                <h3>Payment Link</h3>
+                                <p>{classData.paymentLink}</p>
                             </div>
                         </Col>
                     </Row>
