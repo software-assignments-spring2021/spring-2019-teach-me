d4a35d5b30f34dd6f75bfa48f090fd84699ffac6
derekh2699 <35851184+derekh2699@users.noreply.github.com>
2019-05-02 12:58:42 -0400

    Update about.js

--- src/views/about/about.js
+++ src/views/about/about.js
@@ -3,11 +3,13 @@ import './About.css'
 
 const About = props => {
 	return (
+		<Jumbotron>
 		<div>
 			<h1>About TeachMe</h1>
 			<p>Teach Me is a software project designed to connect users to teach each other creative skills. Teach Me provides creatively skilled individuals (musicians, artists etc.) with an additional source of income, while allowing non-skilled individuals to explore skills that they might be interested in learning.</p>
 			<p className="bold-text">This website is still under consturction. Please come back later :)</p>
 		</div>
+		</Jumbotron>
 	);
 };
 export default About;
